14e5097ea057817d264b183b4d639a7c5672bb0f
joaovbach <joaovbach@github.com>
2023-03-01 14:06:21 -0400

    criando um component h1 hellow world

---
+++ src/componentes/ListaDatas/ListaDatas.js
@@ -0,0 +1,9 @@
+import './ListaDatas.css'
+
+function ListaDatas(props){
+    return(
+        <h1>hello world carraio</h1>
+    );
+};
+
+export default ListaDatas;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ListaDatas from './componentes/ListaDatas/ListaDatas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ListaDatas></ListaDatas>
   </React.StrictMode>
 );
 
